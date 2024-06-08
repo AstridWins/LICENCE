c3f4db23a2ef77b159aff49e560a5da0daf79896
AstridWins <winsouastrid@gmail.com>
2024-06-08 15:46:49 +0100

    hnjgjujgukju

---
+++ index.php
@@ -0,0 +1,220 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+    <title>Compagnie Aérienne</title>
+   
+    <!-- CSS Files -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/plugins/fontawesome/css/fontawesome.min.css">
+    <link rel="stylesheet" href="assets/plugins/fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="assets/css/feather.css">
+    <link rel="stylesheet" href="assets/plugins/select2/css/select2.min.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/plugins/aos/aos.css">
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body class="home-page-five">
+    <div class="main-wrapper">
+        <!-- Hero Section -->
+        <section class="hero-section-five">
+            <header class="header header-five fixed-top">
+                <div class="container">
+                    <nav class="navbar navbar-expand-lg header-nav">
+                        
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNav">
+                            <ul class="navbar-nav mr-auto">
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="#">Accueil</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Rechercher vols</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Réservations</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">En savoir plus</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="admin/index.html">Admin</a>
+                                </li>
+                            </ul>
+                            <ul class="navbar-nav ml-auto">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="choose-signup.html">S'inscrire</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="login.html"><i class="feather-user me-2"></i>Se connecter</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </header>
+            <div class="container">
+                <div class="home-banner home-banner-three">
+                    <div class="row align-items-center w-100">
+                        <div class="col-lg-6 col-md-12 mx-auto">
+                            <div class="section-search section-section-five">
+                                <h1>Bienvenue sur notre plateforme de gestion des passagers</h1>
+                                <div class="search-box search-box-five">
+                                    <form action="search.html" class="search-box-form">
+                                        <div class="search-input">
+                                            <div class="search-group-icon search-group-icon-5">
+                                                <i class="feather-search"></i>
+                                            </div>
+                                            <div class="form-group mb-0">
+                                                <label>Où</label>
+                                                <select class="select">
+                                                    <option>Choisir une destination</option>
+                                                    <option>Toronto</option>
+                                                    <option>Texas</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="floating-img">
+                                <img src="assets/imgs/carousel-1.jpg" alt="image" class="img-fluid rotate-img">
+                            </div>
+                            <div class="car-five-arrow-img">
+                                <img src="assets/img/car-five-arrow.png" alt="image">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Footer -->
+        <footer class="footer footer-five">
+            <div class="footer-top aos" data-aos="fade-up">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-4 col-md-6">
+                            <div class="footer-widget">
+                                <div class="footer-logo">
+                                    <a href="index-3.html"><img src="assets/img/logo-03.png" alt="logo"></a>
+                                </div>
+                                <div class="footer-content">
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.</p>
+                                </div>
+                                <div class="footer-selects">
+                                    <h2 class="footer-title">Langue & Devise</h2>
+                                    <div class="row">
+                                        <div class="col-lg-12 d-flex">
+                                            <div class="footer-select">
+                                                <img src="assets/img/icons/global.svg" alt="icon">
+                                                <select class="select">
+                                                    <option>English</option>
+                                                    <option>Français</option>
+                                                    <option>Español</option>
+                                                </select>
+                                            </div>
+                                            <div class="footer-select">
+                                                <img src="assets/img/icons/dropdown.svg" class="footer-dropdown" alt="icon">
+                                                <select class="select">
+                                                    <option>US Dollars</option>
+                                                    <option>INR</option>
+                                                    <option>Kuwait</option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-2 col-md-6">
+                            <div class="footer-widget footer-menu">
+                                <h2 class="footer-title">Liens Rapides</h2>
+                                <ul>
+                                    <li><a href="about-us.html">À propos de nous</a></li>
+                                    <li><a href="blog-grid.html">Blogs</a></li>
+                                    <li><a href="contact-us.html">Contactez-nous</a></li>
+                                    <li><a href="provider-signup.html">Devenir un professionnel</a></li>
+                                    <li><a href="user-signup.html">Devenir un utilisateur</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6">
+                            <div class="footer-widget footer-contact">
+                                <h2 class="footer-title">Contactez-nous</h2>
+                                <div class="footer-contact-info">
+                                    <div class="footer-address">
+                                        <p><span><i class="feather-map-pin"></i></span> 367 Hillcrest Lane, Irvine, Californie, États-Unis</p>
+                                    </div>
+                                    <p><span><i class="feather-phone"></i></span> 321 546 8764</p>
+                                    <p class="mb-0"><span><i class="feather-mail"></i></span> <a href="mailto:info@compagnieaerienne.com">info@compagnieaerienne.com</a></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6">
+                            <div class="footer-widget">
+                                <h2 class="footer-title">Suivez-nous</h2>
+                                <div class="social-icon">
+                                    <ul>
+                                        <li><a href="#"><i class="fa-brands fa-facebook"></i></a></li>
+                                        <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                        <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+                                        <li><a href="#"><i class="fa-brands fa-linkedin"></i></a></li>
+                                    </ul>
+                                </div>
+                                <h2 class="footer-subtitle">Inscription à la newsletter</h2>
+                                <div class="subscribe-form">
+                                    <input type="email" class="form-control" placeholder="Entrez votre adresse email">
+                                    <button type="submit" class="btn footer-btn">
+                                        <i class="feather-send"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="footer-bottom">
+                <div class="container">
+                    <div class="copyright">
+                        <div class="row align-items-center">
+                            <div class="col-md-4">
+                                <div class="payment-image">
+                                    <ul>
+                                        <li><a href="#"><img src="assets/img/payment/visa.png" alt="img"></a></li>
+                                        <li><a href="#"><img src="assets/img/payment/mastercard.png" alt="img"></a></li>
+                                        <li><a href="#"><img src="assets/img/payment/paypal.png" alt="img"></a></li>
+                                        <li><a href="#"><img src="assets/img/payment/american.png" alt="img"></a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="privacy-content">
+                                    <a href="#">Politique de confidentialité</a>
+                                    <a href="#">Conditions d'utilisation</a>
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="copyright-text">
+                                    <p class="mb-0">&copy; 2023 Compagnie Aérienne. All Rights Reserved.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- /Footer -->
+    </div>
+</body>
+
+</html>
---
+++ test.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil - Compagnie Aérienne</title>
+    
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="assets/plugins/fontawesome/css/fontawesome.min.css">
+    <link rel="stylesheet" href="assets/plugins/fontawesome/css/all.min.css">
+
+    <link rel="stylesheet" href="assets/css/feather.css">
+
+    <link rel="stylesheet" href="assets/plugins/select2/css/select2.min.css">
+
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+
+    <link rel="stylesheet" href="assets/plugins/aos/aos.css">
+
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+    <!-- En-tête -->
+    <header class="header header-five">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg header-nav">
+                <div class="main-menu-wrapper">
+                    <div class="menu-header">
+                        <a href="index.html" class="menu-logo">
+                            <img src="assets/img/logo-02.svg" class="img-fluid" alt="Logo">
+                        </a>
+                        <a id="menu_close" class="menu-close" href="javascript:void(0);">
+                            <i class="fas fa-times"></i></a>
+                    </div>
+                    <ul class="main-nav">
+                        <li class="active">
+                            <a href="index.html">Accueil</a>
+                        </li>
+                        <li>
+                            <a href="rechercher-vol.html">Rechercher Vols</a>
+                        </li>
+                        <li>
+                            <a href="reservations.html">Réservations</a>
+                        </li>
+                        <li>
+                            <a href="contact.html">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+                <ul class="nav header-navbar-rht">
+                    <li class="nav-item">
+                        <a class="nav-link header-reg" href="inscription.html">Inscription</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link header-login" href="connexion.html"><i class="feather-user me-2"></i>Connexion</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <!-- Section Bannière Principale -->
+    <section class="banner">
+        <div class="container">
+            <h1>Bienvenue sur notre plateforme de gestion des passagers</h1>
+            <form class="search-form">
+                <input type="text" placeholder="Aéroport de départ">
+                <input type="text" placeholder="Aéroport d'arrivée">
+                <input type="date" placeholder="Date de départ">
+                <input type="date" placeholder="Date de retour">
+                <input type="number" placeholder="Nombre de passagers">
+                <button type="submit">Rechercher</button>
+            </form>
+        </div>
+    </section>
+
+    <!-- Section Informations Clés -->
+    <section class="info">
+        <div class="container">
+            <h2>Promotions et Offres Spéciales</h2>
+            <!-- Contenu des promotions -->
+
+            <h2>Destinations Populaires</h2>
+            <!-- Vignettes des destinations -->
+        </div>
+    </section>
+
+    <!-- Section Fonctionnalités -->
+    <section class="features">
+        <div class="container">
+            <h2>Nos Services</h2>
+            <div class="feature-item">
+                <h3>Réserver un Vol</h3>
+                <p>Réservez votre vol en quelques clics.</p>
+            </div>
+            <div class="feature-item">
+                <h3>Consulter les Réservations</h3>
+                <p>Consultez et gérez vos réservations.</p>
+            </div>
+            <div class="feature-item">
+                <h3>Imprimer un Ticket</h3>
+                <p>Imprimez votre billet de vol facilement.</p>
+            </div>
+            <div class="feature-item">
+                <h3>Payer une Réservation</h3>
+                <p>Effectuez le paiement de votre réservation en ligne.</p>
+            </div>
+        </div>
+    </section>
+
+    <!-- Section Témoignages -->
+    <section class="testimonials">
+        <div class="container">
+            <h2>Avis Clients</h2>
+            <!-- Témoignages des clients -->
+        </div>
+    </section>
+
+    <!-- Section Actualités -->
+    <section class="news">
+        <div class="container">
+            <h2>Dernières Nouvelles</h2>
+            <!-- Articles et mises à jour -->
+        </div>
+    </section>
+
+    <!-- Pied de page -->
+    <footer>
+        <div class="container">
+            <div class="contact-info">
+                <p>Adresse : 123 Rue de l'Aéroport, Ville, Pays</p>
+                <p>Téléphone : +33 1 23 45 67 89</p>
+                <p>Email : contact@compagnieaerienne.com</p>
+            </div>
+            <div class="useful-links">
+                <a href="#">Conditions Générales</a>
+                <a href="#">Politique de Confidentialité</a>
+                <a href="#">FAQ</a>
+            </div>
+            <div class="social-media">
+                <a href="#"><img src="facebook.png" alt="Facebook"></a>
+                <a href="#"><img src="twitter.png" alt="Twitter"></a>
+                <a href="#"><img src="instagram.png" alt="Instagram"></a>
+            </div>
+        </div>
+    </footer>
+
+
+    <script data-cfasync="false"
+        src="https://truelysell.dreamstechnologies.com/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+    <script src="assets/js/jquery-3.7.0.min.js" type="f017b000c6424141cdfe4e5b-text/javascript"></script>
+
+    <script src="assets/js/bootstrap.bundle.min.js" type="f017b000c6424141cdfe4e5b-text/javascript"></script>
+
+    <script src="assets/js/feather.min.js" type="f017b000c6424141cdfe4e5b-text/javascript"></script>
+
+    <script src="assets/js/owl.carousel.min.js" type="f017b000c6424141cdfe4e5b-text/javascript"></script>
+
+    <script src="assets/plugins/select2/js/select2.min.js" type="f017b000c6424141cdfe4e5b-text/javascript"></script>
+
+    <script src="assets/plugins/aos/aos.js" type="f017b000c6424141cdfe4e5b-text/javascript"></script>
+
+    <script src="assets/js/backToTop.js" type="f017b000c6424141cdfe4e5b-text/javascript"></script>
+
+    <script src="assets/js/script.js" type="f017b000c6424141cdfe4e5b-text/javascript"></script>
+    <script
+        src="https://truelysell.dreamstechnologies.com/cdn-cgi/scripts/7d0fa10a/cloudflare-static/rocket-loader.min.js"
+        data-cf-settings="f017b000c6424141cdfe4e5b-|49" defer></script>
+</body>
+</html>
